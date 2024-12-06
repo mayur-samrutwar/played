63c23ea90bc7a3db45a89c372e43f2c2b1571119
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-12-07 00:00:49 +0530

    add: wallet login

---
+++ components/Layout/index.js
@@ -0,0 +1,12 @@
+import Navbar from '../Navbar';
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col mx-8 my-4">
+      <Navbar />
+      <main className="mt-4 flex-1">
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+
+import Image from 'next/image';
+// import logo from '../public/logo.png';
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center px-4 sm:px-6 w-full border-b pb-4 border-gray-200">
+      <div className="relative h-10 w-40">
+        <Link href="/">
+        {/* <Image
+          src={logo}
+          alt="Logo"
+          fill
+          className="object-contain"
+          priority
+        /> */}
+        play.gg
+        </Link>
+      </div>
+      <div className="flex-grow text-center space-x-8">
+      <Link href="/create" className="text-lg text-gray-700 hover:text-gray-900">
+          Create
+        </Link>
+        <Link href="/market" className="text-lg text-gray-700 hover:text-gray-900">
+          Marketplace
+        </Link>
+        <Link href="/relayer" className="text-lg text-gray-700 hover:text-gray-900">
+          Relayer
+        </Link>
+        <Link href="/profile" className="text-lg text-gray-700 hover:text-gray-900">
+          Profile
+        </Link>
+      </div>
+      <w3m-button label="Login" balance="hide" />
+    </nav>
+  );
+}
