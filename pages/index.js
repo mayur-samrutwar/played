ab8d865abde7ff912fcb255a3e1edca8f9452433
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-12-07 21:38:08 +0530

    update: hehe

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
    <div className="flex flex-col items-center justify-center h-screen">
-     <h1 className="font-geist-sans text-7xl mb-8 font-black text-center max-w-4xl">A fitness app that feels like a game</h1>
-     <button className="font-geist-mono text-lg px-4 py-2 rounded bg-blue-500 text-white border-none">Play Now</button>
+     <h1 className="font-geist-sans text-7xl mb-8 font-black text-center max-w-4xl">wtf? just enjoy the games, man</h1>
    </div>
   );
 }
